70d6e8cd131a7ea9677f1a8b7d87620f5f92c3bc
Pundlik Bhalerao <pundlikbhalerao01@gmail.com>
2025-10-13 20:31:28 +0530

    23-Telephone-formatter

---
+++ 23-telephone-formatter-app/script.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('input')
+let previousValue = ''
+let FirstThreeNumbers = ''
+
+input.addEventListener('input', (e) => {
+    const inputValue = e.target.value
+        if (/\d+$/g.test(inputValue)) {
+        input.value = inputValue
+    } else {
+        input.value = inputValue.substring(0, inputValue.length - 1)
+    }
+
+    if (inputValue.length === 4 && previousValue.length < inputValue.length) {
+        FirstThreeNumbers = inputValue.substring(0, 3)
+        input.value = `+(${FirstThreeNumbers}) - ${inputValue[inputValue.length - 1]}`
+    } else if (inputValue.length === 9 && previousValue.length > inputValue.length) {
+        input.value = FirstThreeNumbers
+    }
+    previousValue = inputValue
+})
