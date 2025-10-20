77bf5cbcd582729b995d64179630ceafd8d96ccc
Pundlik Bhalerao <pundlikbhalerao01@gmail.com>
2025-10-20 19:24:31 +0530

    30-image-editor-app

---
+++ 30-image-editor-app/script.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll(".css input");
+
+inputs.forEach((input) => input.addEventListener("change", update));
+inputs.forEach((input) => input.addEventListener("mousemove", update));
+
+function update() {
+  const change = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + change
+  );
+}
